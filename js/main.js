af5d2bbd2707e7a0988c77e2fa473cec4a37d82e
CaptainXD <orcuttkids4him@gmail.com>
2016-07-14 23:55:11 -0700

    Updated main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,38 @@
+var tagArray = [];
+var dupArray = [];
+var flipped;
+
 $(document).ready(function() {
     $.getJSON('../data.json', function(data) {
-        loadJSON(data);
+        init(data);
+        setInterval(flipTag, 3000);
     });
 });
+
+function init(data) {
+    randomize(data);
+    dupArray = tagArray.slice();
+    $('.front p').html(
+        '<a href="javascript:void(0)">' + dupArray.pop().title + '.</a>'
+    );
+    flipped = false;
+}
+
+function flipTag() {
+    if(dupArray.length === 0) {
+        dupArray = tagArray.slice();
+    }
+    if(flipped) {
+        $('#main').flip();
+        $('.back p').html(
+            '<a href="javascript:void(0)">' + dupArray.pop().title + '.</a>'
+        );
+        flipped = false;
+    } else {
+        $('#main').flip();
+        $('.front p').html(
+            '<a href="javascript:void(0)">' + dupArray.pop().title + '.</a>'
+        );
+        flipped = true;
+    }
+}
