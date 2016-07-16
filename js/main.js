b15aa381c504eab9ef641e0ef0aa8b6c9931beac
CaptainXD <orcuttkids4him@gmail.com>
2016-07-15 22:26:42 -0700

    Updated main.js
    The flip effect actually works now! =)

--- js/main.js
+++ js/main.js
@@ -2,37 +2,52 @@ var tagArray = [];
 var dupArray = [];
 var flipped;
 
+
 $(document).ready(function() {
     $.getJSON('../data.json', function(data) {
         init(data);
-        setInterval(flipTag, 3000);
+        window.setInterval(checkFlip, 2500);
     });
 });
 
 function init(data) {
     randomize(data);
-    dupArray = tagArray.slice();
+    dupArray = tagArray.slice(0);
+    $('#main').flip({trigger: 'manual'});
     $('.front p').html(
+        '<a href="javascript:void(0)">' + dupArray.pop().title + '.</a>'
+    );
+    $('.back p').html(
         '<a href="javascript:void(0)">' + dupArray.pop().title + '.</a>'
     );
     flipped = false;
 }
 
-function flipTag() {
-    if(dupArray.length === 0) {
-        dupArray = tagArray.slice();
+function checkFlip() {
+    var popVal = dupArray.pop();
+    if(typeof popVal !== 'undefined') {
+        flipTag(popVal);
+    } else {
+        dupArray = tagArray.slice(0);
+        popVal = dupArray.pop();
+        flipTag(popVal);
     }
+}
+
+function flipTag(popVal) {
     if(flipped) {
-        $('#main').flip();
-        $('.back p').html(
-            '<a href="javascript:void(0)">' + dupArray.pop().title + '.</a>'
-        );
+        $('#main').flip(false);
+        $("#main").on('flip:done', function() {
+            $('.back p').html(
+                '<a href="javascript:void(0)">' + popVal.title + '.</a>');
+        });
         flipped = false;
     } else {
-        $('#main').flip();
-        $('.front p').html(
-            '<a href="javascript:void(0)">' + dupArray.pop().title + '.</a>'
-        );
+        $('#main').flip(true);
+        $("#main").on('flip:done', function() {
+            $('.front p').html(
+                '<a href="javascript:void(0)">' + popVal.title + '.</a>');
+        });
         flipped = true;
     }
 }
